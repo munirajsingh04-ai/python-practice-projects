d5baded23984e4159d657dfd84acdc22c66f39b3
munirajsingh04-ai <munirajsingh04@gmail.com>
2026-05-26 13:11:16 -0400

    Implement number guessing game
    Added a number guessing game that generates a random target number and prompts the user for guesses.

--- guess-the-number/main.py
+++ guess-the-number/main.py
@@ -1,1 +1,20 @@
+import random
+target = random.randint(1,100)
 
+
+while True:
+    userChoice = int(input("Guess the target or Quit(Q): "))
+    if(userChoice=="Q"):
+        break
+    userChoice = int(userChoice)
+    if(userChoice==target):
+        print("Success:Correct Guess!")
+        break
+    elif(userChoice<target):
+        print("your number is too small guess again...")
+    else:
+         print("your number is too big guess again...")
+
+
+         print("-----GAME OVER-----")
+    
