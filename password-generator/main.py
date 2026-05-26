b041d0772b395a2dfac9e13751f50d97af96278c
munirajsingh04-ai <munirajsingh04@gmail.com>
2026-05-26 13:12:05 -0400

    Add password generator functionality

--- password-generator/main.py
+++ password-generator/main.py
@@ -1,1 +1,10 @@
 second basic project
+import random
+import string
+pass_len=8
+charValues = string.ascii_letters + string.punctuation + string.digits
+
+
+password = "".join([random.choice(charValues) for i in range(pass_len)])
+
+print("your random password is:", password)
